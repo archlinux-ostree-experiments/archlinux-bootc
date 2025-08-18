6f2db585e9cab11d420ed7f4b3ae256366fc9cc0
Marco Hünseler <marco@wuthoehle.de>
2025-08-18 22:39:54 +0200

    create_test_image: allow setting the PACKAGE_TAG

--- create_test_image.sh
+++ create_test_image.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 IMAGE_NAME="localhost/archlinux-bootc"
 IMAGE_TAG="test"
 FULL_IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
+PACKAGE_TAG="testing"
 
 if [ $UID -ne 0 ]; then
   echo "You need to run this script as root"
@@ -16,7 +17,7 @@ cp Containerfile Containerfile.podman
 sed -i 's/MOK.crt,ro/MOK.crt,ro,relabel=shared/g' Containerfile.podman
 
 echo "Building image..."
-podman build -t "$FULL_IMAGE" --secret=id=mokkey,src=MOK.key -f Containerfile.podman .
+podman build -t "$FULL_IMAGE" --build-arg "PACKAGE_TAG=$PACKAGE_TAG" --secret=id=mokkey,src=MOK.key -f Containerfile.podman .
 rm test.img || true
 truncate -s 10G test.img
 podman run --rm --privileged --pid=host --security-opt label=type:unconfined_t -v /dev:/dev -v /var/lib/containers:/var/lib/containers -v .:/output "$FULL_IMAGE" bootc install to-disk --generic-image --filesystem xfs --via-loopback /output/test.img
